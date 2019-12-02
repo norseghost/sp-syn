83cfc40015374865ae9918c73d32c873d6f185c4
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2019-12-02 03:38:15 +0100

    Tilføjer ordfrekvens i pjecen

---
+++ bin/pdf_read.R
@@ -0,0 +1,15 @@
+library('tidyverse')
+library('pdftools')
+library('here')
+library("tidytext")
+
+kl_file <- here("lit/styring-af-det-specialiserede-voksenomraade.pdf")
+stopord <- enframe(read_lines(here("data/stopord.txt")),name = "line", value = "word")
+kl_txt <- pdf_text(kl_file) %>% 
+    enframe(name = "page", value = "text") %>%
+    unnest_tokens(word, text) %>%
+    anti_join(stopord)
+
+kl_wordcount <- kl_txt %>% count(word, sort = TRUE)
+
+kl_data <- pdf_data(kl_file)
