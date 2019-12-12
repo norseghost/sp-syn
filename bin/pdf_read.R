db3047a3dff78911f4e4ce1f778fe354ade77939
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2019-12-12 01:41:24 +0100

    bsdf

--- bin/pdf_read.R
+++ bin/pdf_read.R
@@ -2,7 +2,7 @@ library('tidyverse')
 library('pdftools')
 library('here')
 library("tidytext")
-
+library("tm")
 kl_file <- here("lit/styring-af-det-specialiserede-voksenomraade.pdf")
 stopord <- enframe(read_lines(here("data/stopord.txt")),name = "line", value = "word")
 kl_txt <- pdf_text(kl_file) %>% 
